811bca9d2d990ab479b9db3815a78bdeb2d9fcb7
yama-is-bocchi <yamaa.com987654321@outlook.jp>
2024-03-11 14:48:49 +0900

    プロジェクト ファイルを追加します。

---
+++ guiter_edit_code/Main.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace guiter_edit_code
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Main_panel = new Panel();
+            test = new Label();
+            key_label = new Label();
+            edit_panel = new Panel();
+            convertnum_btn = new Button();
+            song_label = new Label();
+            add_btn = new Button();
+            txt_select_btn = new Button();
+            Main_panel.SuspendLayout();
+            edit_panel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Main_panel
+            // 
+            Main_panel.AutoScroll = true;
+            Main_panel.BackColor = Color.Black;
+            Main_panel.Controls.Add(test);
+            Main_panel.Controls.Add(key_label);
+            Main_panel.Controls.Add(edit_panel);
+            Main_panel.Controls.Add(song_label);
+            Main_panel.Controls.Add(add_btn);
+            Main_panel.Controls.Add(txt_select_btn);
+            Main_panel.Dock = DockStyle.Fill;
+            Main_panel.Location = new Point(0, 0);
+            Main_panel.Margin = new Padding(4, 3, 4, 3);
+            Main_panel.Name = "Main_panel";
+            Main_panel.Size = new Size(979, 514);
+            Main_panel.TabIndex = 0;
+            // 
+            // test
+            // 
+            test.AutoSize = true;
+            test.ForeColor = Color.LightGreen;
+            test.Location = new Point(29, 426);
+            test.Name = "test";
+            test.Size = new Size(0, 15);
+            test.TabIndex = 8;
+            // 
+            // key_label
+            // 
+            key_label.AutoSize = true;
+            key_label.Font = new Font("MS UI Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            key_label.ForeColor = Color.LimeGreen;
+            key_label.Location = new Point(3, 378);
+            key_label.Name = "key_label";
+            key_label.Size = new Size(0, 15);
+            key_label.TabIndex = 7;
+            // 
+            // edit_panel
+            // 
+            edit_panel.Controls.Add(convertnum_btn);
+            edit_panel.Location = new Point(12, 12);
+            edit_panel.Margin = new Padding(4, 3, 4, 3);
+            edit_panel.Name = "edit_panel";
+            edit_panel.Size = new Size(211, 100);
+            edit_panel.TabIndex = 6;
+            edit_panel.Visible = false;
+            // 
+            // convertnum_btn
+            // 
+            convertnum_btn.Cursor = Cursors.Hand;
+            convertnum_btn.FlatStyle = FlatStyle.Flat;
+            convertnum_btn.ForeColor = Color.LimeGreen;
+            convertnum_btn.Location = new Point(4, 11);
+            convertnum_btn.Margin = new Padding(4, 3, 4, 3);
+            convertnum_btn.Name = "convertnum_btn";
+            convertnum_btn.Size = new Size(94, 68);
+            convertnum_btn.TabIndex = 4;
+            convertnum_btn.Text = "数値に変換";
+            convertnum_btn.UseVisualStyleBackColor = true;
+            convertnum_btn.MouseClick += convertnum_btn_MouseClick;
+            // 
+            // song_label
+            // 
+            song_label.AutoSize = true;
+            song_label.Font = new Font("MS UI Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            song_label.ForeColor = Color.LimeGreen;
+            song_label.Location = new Point(327, 12);
+            song_label.Margin = new Padding(4, 0, 4, 0);
+            song_label.Name = "song_label";
+            song_label.Size = new Size(0, 19);
+            song_label.TabIndex = 3;
+            song_label.TextChanged += song_label_TextChanged;
+            // 
+            // add_btn
+            // 
+            add_btn.Cursor = Cursors.Hand;
+            add_btn.FlatStyle = FlatStyle.Flat;
+            add_btn.ForeColor = Color.LimeGreen;
+            add_btn.Location = new Point(12, 247);
+            add_btn.Margin = new Padding(4, 3, 4, 3);
+            add_btn.Name = "add_btn";
+            add_btn.Size = new Size(202, 68);
+            add_btn.TabIndex = 2;
+            add_btn.Text = "テキスト追加プログラム開始";
+            add_btn.UseVisualStyleBackColor = true;
+            add_btn.MouseClick += add_btn_MouseClick;
+            // 
+            // txt_select_btn
+            // 
+            txt_select_btn.Cursor = Cursors.Hand;
+            txt_select_btn.FlatStyle = FlatStyle.Flat;
+            txt_select_btn.ForeColor = Color.LimeGreen;
+            txt_select_btn.Location = new Point(12, 137);
+            txt_select_btn.Margin = new Padding(4, 3, 4, 3);
+            txt_select_btn.Name = "txt_select_btn";
+            txt_select_btn.Size = new Size(202, 68);
+            txt_select_btn.TabIndex = 1;
+            txt_select_btn.Text = "編集対象テキストを選択";
+            txt_select_btn.UseVisualStyleBackColor = true;
+            txt_select_btn.MouseClick += root_foldes_btn_MouseClick;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            ClientSize = new Size(979, 514);
+            Controls.Add(Main_panel);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Main";
+            Text = "Main";
+            Main_panel.ResumeLayout(false);
+            Main_panel.PerformLayout();
+            edit_panel.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel Main_panel;
+        private Button add_btn;
+        private Button txt_select_btn;
+        private Label song_label;
+        private Panel edit_panel;
+        private Button convertnum_btn;
+        private Label key_label;
+        private Label test;
+    }
+}
---
+++ guiter_edit_code/Main.cs
@@ -0,0 +1,267 @@
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace guiter_edit_code
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+        private string? File_path;
+        private List<string> song_lists = new List<string>();
+        private List<string> cur_num_lists = new List<string>();
+        private List<string> cur_sharp_lists = new List<string>();
+
+        private string? cur_key;
+
+        private void root_foldes_btn_MouseClick(object sender, MouseEventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Application.StartupPath.Replace("guiter_edit_code.exe", "") + @"codes\";
+            ofd.Title = "開くファイルを選択してください";
+            ofd.RestoreDirectory = true;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                File_path = ofd.FileName;
+                //ラベルに歌詞を表示
+                write_song();
+
+                insert_cur_arr();
+
+            }
+        }
+
+        private void Init()
+        {
+            song_label.Text = "";
+            test.Text = "";
+            cur_key = "";
+            song_lists = new List<string>();
+            cur_num_lists = new List<string>();
+        }
+
+        private void write_song()
+        {
+
+            Init();
+            if (File_path == null) return;
+            StreamReader sr = new StreamReader(File_path);//フィールドデータ読み取り
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine() + "";
+                    string[] values = line.Split("\n");
+
+                    List<string> lists = new List<string>();
+                    lists.AddRange(values);
+                    foreach (string liner in lists)
+                    {
+                        if (liner.Contains("Original Key："))
+                        {
+
+                            key_label.Text = liner;
+                            string p_l = liner;
+                            cur_key = extract_p_key(ref p_l);
+                            if (cur_key.Length > 2)
+                            {
+                                cur_key = cur_key.Substring(0, 2);
+                            }
+                            test.Text = cur_key;
+                        }
+                        song_label.Text += liner + "\n";
+                        song_lists.Add(liner);
+                    }
+
+                }
+                sr.Close();
+            }
+        }
+
+        private string extract_p_key(ref string p_line)
+        {
+            string? ret_str = p_line;
+
+            string[] ret_list = ret_str.Split(" / ");
+
+            return ret_list[0].Replace("Original Key：", "");
+
+
+        }
+
+        private void add_btn_MouseClick(object sender, MouseEventArgs e)
+        {
+            ProcessStartInfo p = new ProcessStartInfo();
+            p.FileName = @"add_program\Scrayping_guitar_code.exe";
+            Process? process = Process.Start(p);
+            if (process == null) return;
+            process.WaitForExit();
+        }
+
+        private void song_label_TextChanged(object sender, EventArgs e)
+        {
+            if (song_label.Text.Length > 0)
+            {
+                edit_panel.Visible = true;
+            }
+            else
+            {
+                edit_panel.Visible = false;
+            }
+        }
+
+        private void insert_cur_arr()
+        {
+            if (cur_key == null) return;
+            bool existed = false;
+            List<string> temp = new List<string>();
+
+            for (int i = 0; i < 18; i++)
+            {
+                if (Common.readOnlycode[i] == cur_key)
+                {
+                    existed = true;
+                    cur_num_lists.Add(Common.readOnlycode[i]);
+
+                }
+                else if (existed == true)
+                {
+                    cur_num_lists.Add(Common.readOnlycode[i]);
+                }
+                else
+                {
+                    temp.Add(Common.readOnlycode[i]);
+                }
+            }
+
+            if (temp.Count > 0)
+            {
+                foreach (string element in temp)
+                {
+                    cur_num_lists.Add((string)element);
+                }
+            }
+
+            foreach (string s in cur_num_lists)
+            {
+                if (s.Contains("#") || s.Contains("♭"))
+                {
+                    cur_sharp_lists.Add(s);
+                }
+            }
+        }
+
+        private int CountChar(string s, char c)
+        {
+            return s.Length - s.Replace(c.ToString(), "").Length;
+        }
+
+        private List<string> replace_num()
+        {
+            string? pre_line = null;
+            List<string> ret_list = new List<string>();
+
+
+            int eng_count;
+            int bigspace_count;
+            int smallspace_count;
+            int count ;
+
+            foreach (string item in song_lists)
+            {
+
+                eng_count=0;
+                bigspace_count=0;
+                smallspace_count=0;
+                count = 0;
+                string work = item;
+
+                //シャープ♭から変換
+                foreach (string num in Common.num_List)
+                {
+                    if (count == cur_sharp_lists.Count) break;
+
+                    work = work.Replace(cur_sharp_lists[count], num);
+
+                    count++;
+                }
+
+                count = 0;
+                foreach (string num in Common.num_List)
+                {
+                    work = work.Replace(cur_num_lists[count], num);
+
+                    count++;
+                }
+
+
+
+                eng_count = (Regex.Replace(item, "[^a-zA-Z]", "")).Length;
+                bigspace_count = CountChar(item, '　');
+                smallspace_count = CountChar(item, ' ');
+
+                if (pre_line == null)
+                {
+                    ret_list.Add(item);
+                }
+                else if (item.Contains("#") || item.Contains("♭") || item.Contains("/")|| item.Contains("7"))
+                {
+                    ret_list.Add(work);
+                }
+                else if (eng_count>8)
+                {
+                    ret_list.Add(item);
+                }
+                else if (eng_count < 5)
+                {
+                    ret_list.Add(work);
+                }
+                else if (bigspace_count > 4 || smallspace_count > 4)
+                {
+                    ret_list.Add(work);
+                }
+                else
+                {
+                    ret_list.Add(item);
+                }
+
+                pre_line = item;
+            }
+
+
+            return ret_list;
+        }
+
+        private void convertnum_btn_MouseClick(object sender, MouseEventArgs e)
+        {
+            List<string> write_list = replace_num();
+            song_label.Text = "";
+            if (File_path == null) return;
+            using (StreamWriter sw = new StreamWriter(File_path.Replace(".txt", "_numver.txt"), false,
+                                Encoding.GetEncoding("utf-8")))
+            {
+                foreach (string item in write_list)
+                {
+
+                    sw.WriteLine(item);
+                    song_label.Text += item+"\n";
+
+                }
+
+                sw.Close();
+            }
+            test.Text = "完了しました";
+        }
+    }
+}
